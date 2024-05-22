980e1cef60d622f1ca4f07ebcf1e7d4bc406d31c
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-05-22 15:49:53 +0300

    Implement WebAppProject.Commen

---
+++ WebAppProject.Common/ModelsValidationConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppProject.Common
+{
+    public class ModelsValidationConstants
+    {
+        public static class Product
+        {
+            public const int NameMaxLength = 50;
+            public const int NameMinLength = 1;
+
+            public const string PriceMax = "1000";
+            public const string PriceMin = "0";
+        }
+
+
+    }
+}
