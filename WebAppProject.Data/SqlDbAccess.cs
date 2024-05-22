c3f92e99b884bf2b424577f233a459e8dd9fe1f9
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-05-22 07:37:31 +0300

    Create SqlDbAccess and using dapper

---
+++ WebAppProject.Data/SqlDbAccess.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+
+namespace WebAppProject.Data
+{
+    public class SqlDbAccess
+    {
+        private readonly IConfiguration config;
+
+        public string ConnnectionStringName { get; set; } = "Default";
+
+        public SqlDbAccess(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionString = this.config.GetConnectionString(ConnnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+
+                return data.ToList();
+            }
+        }
+
+        
+    }
+}
