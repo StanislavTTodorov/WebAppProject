4ddf888684efc2e1f59cc2bf6a0ac1bceecf50d3
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-05-26 10:30:36 +0300

    fix Commen

--- WebAppProject.Common/ModelsValidationConstants.cs
+++ WebAppProject.Common/ModelsValidationConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebAppProject.Common
+﻿namespace WebAppProject.Common
 {
     public class ModelsValidationConstants
     {
@@ -13,10 +7,8 @@ namespace WebAppProject.Common
             public const int NameMaxLength = 50;
             public const int NameMinLength = 1;
 
-            public const string PriceMax = "1000";
+            public const string PriceMax = "1000000";
             public const string PriceMin = "0";
         }
-
-
     }
 }
