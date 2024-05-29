9c2d705ab824f72cc4cb265d115f5d1546002b82
Stanislav Todorov <77863355+StanislavTTodorov@users.noreply.github.com>
2024-05-29 09:16:56 +0300

    Implement ProductViewModel

---
+++ WebAppProject.ViewModels/Product/IProductsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Blazor;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WebAppProject.Data.Models;
+
+namespace WebAppProject.ViewModels.Product
+{
+    public interface IProductsViewModel
+    {
+        List<ProductModel> Products { get; }
+
+        void CustomizeEditModel(GridCustomizeEditModelEventArgs e);
+        void DeletingProduct(GridDataItemDeletingEventArgs e);
+        void EditProduct(GridEditModelSavingEventArgs e);
+    }
+}
---
+++ WebAppProject.ViewModels/Product/ProductsViewModel.cs
@@ -0,0 +1,78 @@
+﻿
+
+
+using DevExpress.Blazor;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using WebAppProject.Data;
+using WebAppProject.Data.Models;
+
+namespace WebAppProject.ViewModels.Product
+{
+    
+    public class ProductsViewModel : IProductsViewModel
+    {
+        private readonly IProductDataService productDataService;
+
+        public  ProductsViewModel(IProductDataService productDataService)
+        {
+            this.productDataService = productDataService;
+
+            Task.Run(async () =>
+            {
+                products = await this.productDataService.GetProducts();
+            })
+            .GetAwaiter()
+            .GetResult();
+
+        }
+
+
+
+        private List<ProductModel> products = new List<ProductModel>();
+
+        public List<ProductModel> Products
+        { 
+            get
+            {
+                return products;
+            }
+        }
+
+        public async void DeletingProduct(GridDataItemDeletingEventArgs e)
+        {
+            products.Remove(e.DataItem as ProductModel);
+            await this.productDataService.DeleteProduct(e.DataItem as ProductModel);
+        }
+
+        public async void EditProduct(GridEditModelSavingEventArgs e)
+        {
+            ProductModel editModel = (ProductModel)e.EditModel;
+            ProductModel newProduct = e.IsNew ? new ProductModel() : (ProductModel)e.DataItem;
+
+            newProduct.Name = editModel.Name;
+            newProduct.Price = editModel.Price;
+            newProduct.DateAdded = editModel.DateAdded;
+
+            if (e.IsNew)
+            {
+                newProduct.Id = Guid.NewGuid();
+                products.Add(newProduct as ProductModel);
+                await this.productDataService.InsertProduct(newProduct);
+            }
+            else 
+            {
+                await this.productDataService.UpdateProduct(newProduct);
+            }
+        }
+
+        public void CustomizeEditModel(GridCustomizeEditModelEventArgs e)
+        {
+            if (e.IsNew)
+            {
+                var editModel = (ProductModel)e.EditModel;
+                editModel.DateAdded = DateTime.UtcNow;
+            }
+        }
+    }
+}
